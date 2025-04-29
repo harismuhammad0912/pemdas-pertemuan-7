e27131e3fe75be3035fb2d36fabfa7881f201256
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-04-29 11:06:02 +0700

    pembuatan class

--- intro.cpp
+++ intro.cpp
@@ -8,20 +8,5 @@ class mahasiswa{
     string nama;
     float nilai;
 
-    void print_date(){
-        cout << "NIM    = " << nim << endl;
-        cout << "Nama    = " << nama << endl;
-        cout << "Nilai    = " << nilai << endl;
-    }
-};
-
-int main(){
-    mahasiswa mhs;
-    mhs.nim = 2023;
-    mhs.nama = "Gyan";
-    mhs.nilai = 99.9;
-
-    mhs.print_date();
 }
-
 
