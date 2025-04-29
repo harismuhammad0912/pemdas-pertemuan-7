981c882ae5caa87d16aa39b109a0d083aac82fd5
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-04-29 11:02:49 +0700

    1

---
+++ intro.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+
+    public:
+    int nim;
+    string nama;
+    float nilai;
+
+    void print_date(){
+        cout << "NIM    = " << nim << endl;
+        cout << "Nama    = " << nama << endl;
+        cout << "Nilai    = " << nilai << endl;
+    }
+};
+
+int main(){
+    mahasiswa mhs;
+    mhs.nim = 2023;
+    mhs.nama = "Gyan";
+    mhs.nilai = 99.9;
+
+    mhs.print_date();
+}
+
+
